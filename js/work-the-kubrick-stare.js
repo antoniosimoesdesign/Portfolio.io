87ddbf68139dfb3251ae7b4754517a7fac046951
antoniooooooooooooooooo <apedrosimoes39@gmail.com>
2024-07-18 13:35:10 +0100

    ok

--- js/work-the-kubrick-stare.js
+++ js/work-the-kubrick-stare.js
@@ -10,6 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 const image2Url = work.metadata.image2.url;
                 const image3Url = work.metadata.image3.url;
                 const image4Url = work.metadata.image4.url;
+                const image5Url = work.metadata.image5.url;
+                const image6Url = work.metadata.image6.url;
+                const image7Url = work.metadata.image7.url;
+                const image8Url = work.metadata.image8.url;
+                const image9Url = work.metadata.image9.url;
+                const image10Url = work.metadata.image10.url;
+                const image11Url = work.metadata.image11.url;
+                const image12Url = work.metadata.image12.url;
 
                 // Update the DOM with fetched data
                 document.getElementById('work-title').textContent = title;
@@ -18,6 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById('work-image-2').src = image2Url;
                 document.getElementById('work-image-3').src = image3Url;
                 document.getElementById('work-image-4').src = image4Url;
+                document.getElementById('work-image-5').src = image5Url;
+                document.getElementById('work-image-6').src = image6Url;
+                document.getElementById('work-image-7').src = image7Url;
+                document.getElementById('work-image-8').src = image8Url;
+                document.getElementById('work-image-9').src = image9Url;
+                document.getElementById('work-image-10').src = image10Url;
+                document.getElementById('work-image-11').src = image11Url;
+                document.getElementById('work-image-12').src = image12Url;
             } else {
                 console.error('Work not found.');
             }
